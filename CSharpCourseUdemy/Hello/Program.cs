ad1b745b00a42b60b7c03ba6def1b79accca1cf1
zsalach <61181802+zsalach@users.noreply.github.com>
2020-02-23 22:08:36 +0100

    Hello2. Tablica args

--- CSharpCourseUdemy/Hello/Program.cs
+++ CSharpCourseUdemy/Hello/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Hello
 {
@@ -10,7 +7,17 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            /*
+             * string name = args[0];
+            string surname = args[1];
+            string nickname = args[2];
+            Console.WriteLine($"Witaj {name} {surname} {nickname}!" );
+            */
+
+            foreach (var item in args)
+            {
+                Console.WriteLine($"Witaj {item}");
+            }
         }
     }
 }
