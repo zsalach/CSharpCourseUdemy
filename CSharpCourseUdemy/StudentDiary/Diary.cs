a8f46c5c8c7d03e54a37e6ceeb98aeb103bcb363
zsalach <61181802+zsalach@users.noreply.github.com>
2020-02-24 14:02:12 +0100

    diary

---
+++ CSharpCourseUdemy/StudentDiary/Diary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // stan (zmienne - pola)
+
+        List<float>ratings = new List<float>();
+
+
+        //zachowania
+        public void AddRating(float rating) // metoda ma dodawać oceny di naszej listy.
+        {
+            ratings.Add(rating);
+        }
+        public float CalculateAverage()
+        {
+            float sum = 0;
+            float avg = 0;
+
+            foreach(var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+
+
+        // metoda do pobierania maksymalnej oceny..
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GivrMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ CSharpCourseUdemy/StudentDiary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+           
+/*          diary.AddRating(5);
+            diary.AddRating(8.5f);       
+            diary.AddRating(8.7f);
+            float avg = diary.CalculateAverage();
+            float max = diary.GiveMaxRating();
+            float min = diary.GivrMinRating();
+*/
+
+            for(; ; ) //nieskończona pętla!!!
+            {
+                Console.WriteLine("Podaj ocenę z zakresu 1 - 10");
+                float rating; 
+                bool result = float.TryParse(Console.ReadLine(), out rating); 
+                // sprawdza czy wprowadzony znak list liczbą
+                
+                if(rating == 11)    // program jes zakończany po wpisaniu liczby 11
+                {
+                    break;
+                }
+
+                if(result = true) // jeżeli liczba to
+                {
+                    if (rating > 0 && rating <=10) // sprawdzamy czy jest z przedziału 1-10
+                    {
+                        diary.AddRating(rating); // jeżeli z przedziału to dodajemy.
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna liczba");
+                    }
+                }
+            } 
+            // po zakończeniu pętli wyświetla dane.
+            Console.WriteLine($"Srednia twoich ocen to: {diary.CalculateAverage()}");
+            Console.WriteLine("Najwyższa oczena to: {0}",diary.GiveMaxRating());
+            Console.WriteLine($"Najniższa ocena to: {diary.GivrMinRating()}");
+            Console.WriteLine();
+        }
+    }
+}
